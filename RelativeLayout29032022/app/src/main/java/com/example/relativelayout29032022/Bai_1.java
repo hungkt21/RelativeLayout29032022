aab08611aa3255f763ad0ae88d3b3194a8ecbd2c
unknown <hungkt21@gmail.com>
2022-04-26 22:23:40 +0700

    Bai tap RelativeLayout

---
+++ RelativeLayout29032022/app/src/main/java/com/example/relativelayout29032022/Bai_1.java
@@ -0,0 +1,14 @@
+package com.example.relativelayout29032022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai_1);
+    }
+}
